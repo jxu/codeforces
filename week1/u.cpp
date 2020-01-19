d90d8efaab42af0518727b7bfdd40cd9becfe40f
jxu <7989982+jxu@users.noreply.github.com>
2020-01-19 00:52:28 -0500

    Add correct method for uranium cubes

--- week1/u.cpp
+++ week1/u.cpp
@@ -4,43 +4,64 @@
 
 using namespace std;
 
+// test if subset size s is valid using sliding window and median
+bool valid_subset_size(const vector<int>& a, int n, int s, long long B)
+{
+    int lo = 0, m = (s-1)/2, hi = s-1;
+    long long cost = 0;
+    for (int i=lo; i<=hi; i++)
+    {
+        cost += abs(a[i] - a[m]);
+    }
+
+    for ( ; hi <= n-1; lo++, hi++, m++)
+    {
+        //cout << "cost: " << cost << '\n';
+        if (cost <= B) return true;
+        
+        if (hi < n-1) 
+        {
+            cost += a[lo] + a[hi+1] - a[m+1] - (s%2 ? a[m] : a[m+1]);
+        }
+    }
+    return false;
+}
+
 int main() 
 {
     int n, L;
     long long B;
-    vector<int> v, d;
+    vector<int> a, d;
 
     cin >> n >> L >> B;
     for (int i=0; i<n; i++)
     {
         int x;
         cin >> x;
-        v.push_back(x);
+        a.push_back(x);
     }
 
-    sort(v.begin(), v.end());
-    int p = v[v.size() / 2];
-    
-    for (int x : v)
+    sort(a.begin(), a.end());
+
+
+    // binary search s
+    int s_lo = 1, s_hi = n, s_mid;
+
+    while (s_lo < s_hi) 
     {
-        d.push_back(abs(x - p));
-        cout << abs(x-p) << ' ';
-        cout << '\n';
+        s_mid = (s_lo + s_hi + 1)/2;
+        //cout << s_lo << " " << s_mid << " " << s_hi << '\n';
+        //cout << valid_subset_size(a, n, s_mid, B) <<'\n';
+        if (valid_subset_size(a, n, s_mid, B))
+        {
+            s_lo = s_mid;
+        } else
+        {
+            s_hi = s_mid - 1;
+        }
     }
 
-    sort(d.begin(), d.end());
+    cout << s_lo << '\n';
 
-    int s = 0;
-    long long b = 0;
-
-    for (int i=0; i<n; i++)
-    {
-        if (b + d[i] > B) break;
-        s++;
-        b += d[i];
-
-    }
-
-    cout << s << '\n';
-    
+    return 0;
 }
