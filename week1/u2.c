5ab0986b0fb1f4ec0f0f629ab1ad61c41d5bbd99
jxu <7989982+jxu@users.noreply.github.com>
2020-01-21 19:08:53 -0500

    Add C version of linear time uranium cubes code

--- week1/u2.c
+++ week1/u2.c
@@ -1,26 +1,29 @@
 // 2 pointers linear time algorithm
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
 
-using namespace std;
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+int cmp_ints(const void* a, const void* b)
+{
+    int arg1 = *(const int*)a;
+    int arg2 = *(const int*)b;
+    return (arg1 > arg2) - (arg1 < arg2);
+}
+
+int a[100000];
 
 int main() 
 {
     int n, L;
     long long B;
-    vector<int> a;
+    scanf("%d %d %lld", &n, &L, &B);
 
-    cin >> n >> L >> B;
     for (int i=0; i<n; i++)
-    {
-        int ai;
-        cin >> ai;
-        a.push_back(ai);
-    }
+        scanf("%d", &(a[i]));
 
-    sort(a.begin(), a.end());
+    qsort(a, n, sizeof(int), cmp_ints); 
 
     int lo = 0, m = 0, hi = 0;
     int best_s = 1;
@@ -47,17 +50,17 @@ int main()
         /*
         for (int i=lo; i<=hi; i++)
         {
-            cout << a[i];
-            if (i == m) cout << '*';
-            cout << ' ';
+            printf("%d", a[i]);
+            if (i == m) printf("*");
+            printf(" ");
         }
-        cout << "cost " << cost << endl;
+        printf("cost %lld\n", cost);
         */
 
-        best_s = max(best_s, (hi - lo + 1));
+        best_s = MAX(best_s, (hi - lo + 1));
     }
 
-    cout << best_s << endl;
+    printf("%d\n", best_s);
 
     return 0;
 }
