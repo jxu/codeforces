7d0dae6f0789472b2ddc898bc9aba64da1c7bb2d
jxu <7989982+jxu@users.noreply.github.com>
2020-01-26 20:55:00 -0500

    Cleanup original destroying arrays

--- week2/d1.cpp
+++ week2/d1.cpp
@@ -8,12 +8,6 @@ using namespace std;
 
 typedef long long ll;
 
-// inclusive bounds [lb, hb]
-ll compute_seg_sum(const vector<ll>& prefix_sum, int lb, int ub)
-{
-    return prefix_sum[ub] - ((lb > 0) ? prefix_sum[lb-1] : 0); 
-}
-
 int main() 
 {
     ios_base::sync_with_stdio(false);
@@ -21,60 +15,61 @@ int main()
 
     int n;
     cin >> n;
-    vector<int> v(n), destroyed(n, false);
+    vector<int> v(n);
     vector<ll> prefix_sum(n);
     
     for (int i=0; i<n; i++)
     {
         cin >> v[i];
-        prefix_sum[i] = v[i] + ((i == 0) ? 0 : prefix_sum[i-1]);
+        prefix_sum[i] = v[i];
+        if (i > 0) prefix_sum[i] += prefix_sum[i-1];
     }
 
-    map<int, ll> seg_sum;
-    multiset<ll> sums; // allow multiple equal seg sums
-    seg_sum[n] = prefix_sum[n-1];
-    sums.insert(prefix_sum[n-1]);
+    map<int, ll> segmap;
+    multiset<ll> segsum; // allow multiple equal seg sums
+    segmap[n] = prefix_sum[n-1];
+    segsum.insert(prefix_sum[n-1]);
 
     for (int i=0; i<n; i++)
     {
         int x;
-        cin >> x;
-        --x;
+        cin >> x; --x;
 
-        destroyed[x] = true;
-        int seg_in = (*(seg_sum.upper_bound(x))).first;
-       
-        seg_sum[x] = seg_sum[seg_in] - 
-            compute_seg_sum(prefix_sum, x+1, seg_in-1) - v[x]; 
+        int seg_in = (*(segmap.upper_bound(x))).first;
 
-        // erase once 
-        sums.erase(sums.lower_bound(seg_sum[seg_in]));
+        // erase once
+        segsum.erase(segsum.lower_bound(segmap[seg_in]));
+        
+        ll hi_segsum = prefix_sum[seg_in-1];
+        if (x > 0) hi_segsum -= prefix_sum[x-1];
 
-        seg_sum[seg_in] -= seg_sum[x] + v[x];
-        sums.insert(seg_sum[x]);
-        sums.insert(seg_sum[seg_in]);
+        segmap[x] = segmap[seg_in] - hi_segsum; 
+        segmap[seg_in] -= segmap[x] + v[x];
+        
+        segsum.insert(segmap[x]);
+        segsum.insert(segmap[seg_in]);
    
         #ifdef DEBUG
         cout << endl;
 
         for (int j=0; j<n; j++)
         {
-            if (destroyed[j]) cout << "* ";
+            if (segmap.count(j)) cout << "* ";
             else cout << v[j] << " ";
         }
         cout << endl;
 
         cout << "x:" << x << " seg in:" << seg_in << endl;
 
-        for (auto p : seg_sum)
+        for (auto p : segmap)
             cout << p.first << ":" << p.second << " ";
-        cout << endl << "sums ";
+        cout << endl << "segsum ";
 
-        for (int s : sums)
+        for (ll s : segsum)
             cout << s << " ";
         cout << endl;
         #endif 
 
-        cout << *(sums.rbegin()) << "\n";
+        cout << *(segsum.rbegin()) << "\n";
     }
 }
