64146c1978ff4c422de24e73f99c55a47a669dcd
jxu <7989982+jxu@users.noreply.github.com>
2020-01-28 18:24:25 -0500

    Fix generating sets to use lazy reducing method

--- week2/c.cpp
+++ week2/c.cpp
@@ -10,51 +10,33 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    set<int> s;
-
-    int n;
-    cin >> n;
+    set<int> X;
+    int n; cin >> n;
+    
     for (int i=0; i<n; i++)
     {
-        int y;
-        cin >> y;
-        s.insert(y);
+        int y; cin >> y;
+        X.insert(y);
     }
 
-    bool reduced = false;
-
-    for (auto it=s.rbegin(); it != s.rend(); it++)
-        cout << *it << " ";
-    cout << endl;
-
-    do 
+    while (true)
     {
-        reduced = false;
-        for (auto it = s.rbegin(); it != s.rend(); it++)
+        int Xmax = *(X.rbegin());
+        int x = Xmax;
+
+        while (X.count(x)) 
+            x /= 2;
+
+        if (x == 0) // no place for x
+            break;
+        else
         {
-            int y = *it;
-            int y_new = (y > 1) ? (y - (y%2)) / 2 : y;
-
-            if (!s.count(y_new))
-            {
-                cout << "reduce " << y << " to " << y_new << "\n";
-                s.erase(y);
-                s.insert(y_new);
-                reduced = true;
-
-                for (int yi : s)
-                    cout << yi << " ";
-                cout << "\n";
-
-                break;
-            }
-
+            X.erase(Xmax); 
+            X.insert(x);
         }
+    }
 
-    } while (reduced);
-
-    for (int y : s)
-        cout << y << " ";
+    for (int x : X)
+        cout << x << " ";
     cout << "\n";
-
 }
