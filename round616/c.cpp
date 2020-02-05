9a45061bd01630f71ce736a8762b351e5b3d819a
jxu <7989982+jxu@users.noreply.github.com>
2020-02-05 15:46:51 -0500

    Clean up mind control code

--- round616/c.cpp
+++ round616/c.cpp
@@ -23,46 +23,31 @@ int main()
 
         int x = 0;
 
-
-        if (m <= k) 
+        if (m <= k) // we fix m-1 people, then our pick 
         {
-            for (int i=0; i < m; i++)
+            for (int i=0; i < m; i++) // i from front, ii from back
             {
                 int ii = m-1-i;
                 x = max(x, max(a[i], a[n-1-ii])); 
-
             }
         }
-
-        else
+        else // we fix k people, nature fixes m-k-1, we pick last
         {
-
             for (int i=0; i <= k; i++)
             {
                 int ii = k-i;
-                //cout << "my front/back " << i << " " << n-1 - ii << endl;
-                int best_m = 1 << 30;
+                int y = 1 << 30;
 
                 for (int j=0; j<=m-k-1; j++)
                 {
                     int jj = m-k-1-j;
-
-                    //cout << "nature front/back " << i+j << " " << n-1-ii - jj << endl;
-                    best_m = min(best_m, max(a[i+j], a[n-1-ii-jj]));
-
-
+                    int my = max(a[i+j], a[n-1-ii-jj]); // our pick
+                    y = min(y, my); // nature minimizes
                 }
 
-                //cout << "best m " << best_m << endl;
-
-                x = max(x, best_m);
-
+                x = max(x, y); // we maximize
             }
         }
         cout << x << endl;
-
-
-
     }
-
 }
