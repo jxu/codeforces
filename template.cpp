4cf698bbe7e6f17190a69c8b4d265bdac175efb0
jxu <7989982+jxu@users.noreply.github.com>
2020-02-13 11:36:28 -0500

    Add climits and test cases loop to template

--- template.cpp
+++ template.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
 typedef vector<int> vi;
 typedef vector<vi> vvi;
+typedef long long ll;
+
 
 int main() 
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int t; cin >> t;
+    for (int T=0; T<t; T++)
+    {
+
+
+    }
 }
