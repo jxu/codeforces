149aee2bfcdb967cba6d4534690bfb13760517b3
jxu <7989982+jxu@users.noreply.github.com>
2020-02-24 19:48:50 -0500

    Cleanup WeirdSort a bit

--- round624/b.cpp
+++ round624/b.cpp
@@ -20,50 +20,43 @@ int main()
     {
         int n, m;
         cin >> n >> m;
-        vi a(n), s(n, 0);
+        vi a(n);
+        vector<bool> s(n, false);
+        
         for (int i=0; i<n; i++)
-        {
             cin >> a[i]; 
-        }
-
+        
         for (int i=0; i<m; i++)
         {
-            int pi;
-            cin >> pi;
-            s[pi-1] = 1; 
+            int pi; cin >> pi;
+            s[pi-1] = true; 
         }
 
         vi as = a;
         sort(as.begin(), as.end());
-
        
         bool bad = false;
         for (int i=0; i<n; i++)
         {
-            int j=i;
-            if (a[i] >= as[i]) 
+            if (a[i] == as[i]) continue;
+            if (a[i] > as[i]) 
             {
-                for (; j<n; j++)
+                for (int j = i+1; j<n; j++)
                 {
-                    if (j > i && !s[j-1]) bad = true;
+                    if (!s[j-1]) bad = true;
                     if (a[i] == as[j]) break;
                 }
             } 
             else
             {
-                for (; j >= 0; j--)
+                for (int j = i-1; j>=0; j--)
                 {
-                    if (j < i && !s[j]) bad = true;
+                    if (!s[j]) bad = true;
                     if (a[i] == as[j]) break;
                 }
             }
-
-            //cout << "i j " << i << j << endl;
-
-
         }
 
         cout << ((bad) ? "NO" : "YES") << endl;
-
     }
 }
