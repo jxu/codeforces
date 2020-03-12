a76d08ecdd424a2787ec407d253e597a0c541b29
jxu <7989982+jxu@users.noreply.github.com>
2020-03-12 18:03:02 -0400

    Super simple frog jumps solution :(

--- round627/c.cpp
+++ round627/c.cpp
@@ -9,58 +9,6 @@ typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef long long ll;
 
-bool f(string s, vi r, int n, int d) 
-{
-    bool visited = true ;
-    while (visited) 
-    {
-        //for (int x : r) 
-        //    cout << x;
-        //cout << endl;
-
-        visited = false;
-
-        for (int i=0; i<n+1; i++) 
-        {
-            if (r[i] == 1) 
-            {
-                visited = true;
-                r[i] = 2;
-
-                if (i > 0 && s[i-1] == 'L') 
-                {
-                    for (int j=max(0,i-d); j <= i-1; j++)
-                    {
-                        if (r[j] == 0) {
-                            r[j] = 1; 
-                        }
-                    }
-
-                }
-                else // i == 0 
-                {
-                    for (int j=i+1; j <= min(n+1, i+d); j++)
-                    {
-                        if (r[j] == 0) 
-                        {
-                            r[j] = 1;
-                        }
-                    }
-
-                }
-
-            }
-
-        }
-
-        if (r[n+1] >= 1) return true;
-
-    }
-
-    return false;
-
-
-}
 
 int main() 
 {
@@ -70,29 +18,22 @@ int main()
     int t; cin >> t;
     for (int T=0; T<t; T++)
     {
-        string s;
-        cin >> s;
+        string s; cin >> s;
         int n = (int)s.size();
 
-        vi r(n+2, 0);
-        r[0] = 1;
-        
-      
-        int lo = 0, hi = n+1;
-
-        while (hi - lo > 1) 
+        s.insert(0, "R");
+        s.push_back('R');
+        int d = 0, j = 0;
+        for (int i = 0; i <= n+1; i++)
         {
-            int mid = (lo + hi)/2;
-
-            if (f(s, r, n, mid)) 
-                hi = mid;
-            else
-                lo = mid;
-
+            ++j;
+            if (s[i] == 'R')
+            {
+                d = max(d, j);
+                j = 0;
+            }
         }
-            cout << hi << endl;
 
-        //break;
+        cout << d << endl;
     }
-
 }
