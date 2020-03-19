56e64ea1f5b855fa763186fecd3b25dedffc8eba
jxu <7989982+jxu@users.noreply.github.com>
2020-03-19 00:51:33 -0400

    Modify tenuous triangles for on-the-fly calculation

--- week9/c.cpp
+++ week9/c.cpp
@@ -18,36 +18,17 @@ int main()
     ll n; char c;
     cin >> n;
 
-    vvi adj(n, vi(n, 0));
+    ll res = n * (n-1) * (n-2) / 6;
 
     for (int i=0; i<n; i++)
     {
+        int outdeg = 0;
         for (int j=0; j<n; j++)
         {
             cin >> c;
-            if (c == 'Y') adj[i][j] = 1;
-            //cout << adj[i][j];
+            if (c == 'Y') ++outdeg;
         }
-        //cout << endl;
-
+        res -= outdeg * (outdeg-1) / 2;
     }
-
-    vi indeg(n, 0);
-
-    ll res = n * (n-1) * (n-2) / 6;
-
-    for (int j=0; j<n; j++)
-    {
-        for (int i=0; i<n; i++)
-            if (adj[i][j]) ++indeg[j];
-
-        //cout << indeg[j] << endl;
-        
-        res -= indeg[j] * (indeg[j]-1) / 2;
-    }
-
-
     cout << res << endl;
-
-
 }
