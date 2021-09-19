c6c4ec458269c263d98080d097360a6194fbc376
jxu <7989982+jxu@users.noreply.github.com>
2021-09-18 21:39:06 -0400

    Add one string per line to template and clean up 743a

--- round743/a.py
+++ round743/a.py
@@ -1,12 +1,10 @@
 from sys import stdin
-
-lines = stdin.readlines()
-t= int(lines[0])
-for l in range(1, len(lines), 2):
-    n = int(lines[l])
-    s = lines[l+1].strip()
-    #print(n,s)
+lines = [line.strip() for line in stdin]
+t = int(lines[0])
+ln = 1
+for _ in range(t):
+    n, s = int(lines[ln]), lines[ln+1]
+    ln += 2
     sw = (n-1 - s[:-1].count("0"))
-    print(sw + sum(int(d) for d in s))
-
+    print(sw + sum(map(int, s)))
 
--- template.py
+++ template.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 from sys import stdin
-lines = [list(map(int, line.strip().split())) for line in stdin]
 
+#lines = [line.strip() for line in stdin] # one string per line
+#t = int(lines[0])
+lines = [list(map(int, line.strip().split())) for line in stdin]
 t = lines[0][0]
 ln = 1
 for _ in range(t):
-    n, a, b = lines[ln][0], lines[ln+1]
+    n, a = lines[ln][0], lines[ln+1]
     ln += 2
+    print(n, a)
 
