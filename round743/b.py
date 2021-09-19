5b8e70f7ae3ea24fa541e8e165a79bae35965b4c
jxu <7989982+jxu@users.noreply.github.com>
2021-09-18 21:11:59 -0400

    Clean up python input

--- round743/b.py
+++ round743/b.py
@@ -1,28 +1,22 @@
 from sys import stdin
-
-lines = stdin.readlines()
-t= int(lines[0])
-for l in range(1, len(lines), 3):
-    n = int(lines[l])
-    a = list(map(int, lines[l+1].strip().split()))
-    b = list(map(int, lines[l+2].strip().split()))
-    m = [0] * (2*n+1)
+lines = [list(map(int, line.strip().split())) for line in stdin]
+t = lines[0][0]
+ln = 1
+for t_ in range(t):
+    n, a, b = lines[ln][0], lines[ln+1], lines[ln+2]
+    ln += 3
+    dist = [0]*(2*n+1)
     for i in range(n):
-        m[a[i]] = i
-        m[b[i]] = i
+        dist[a[i]] = i
+        dist[b[i]] = i
 
-    s = [1e7]*(n+1)
-    #print(m)
-
-    for i in range(1,n+1):
-        s[i] = min(m[2*i-1], s[i-1])
-        
-    #print(s[1:])
+    best = [1e7]*(n+1)
 
     for i in range(1, n+1):
-        s[i] += m[2*i]
+        best[i] = min(dist[2*i-1], best[i-1])
 
-    #print(s[1:])
+    for i in range(1, n+1):
+        best[i] += dist[2*i]
 
-    print(min(s[1:]))
+    print(min(best))
 
