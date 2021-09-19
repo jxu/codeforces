bf246dbcf225ffc92257c1e3fb885f1c84e1ffbd
jxu <7989982+jxu@users.noreply.github.com>
2021-09-18 21:54:30 -0400

    Clean up f21 week3 c

--- f21week3/c.py
+++ f21week3/c.py
@@ -1,26 +1,15 @@
 from sys import stdin
 from bisect import bisect_left
 
-lines = stdin.readlines()
-n = int(lines[0])
-b = []
+lines = [list(map(int, line.strip().split())) for line in stdin]
+n = lines[0][0]
 dp = [0]*n
+pairs = sorted(lines[1:])
+pos, pow = zip(*pairs)
 
-for line in map(str.strip, lines[1:]):
-    x, y = map(int, line.strip().split())
-    b.append((x,y))
-
-b.sort()
-
-pos = [x[0] for x in b]
-pow = [x[1] for x in b]
-
-for i in range(1,n):
+for i in range(n):
     j = bisect_left(pos, pos[i]-pow[i])
-    if j == 0: dp[i] = i
-    else: dp[i] = dp[j-1] + i-j
-
-#print(dp)
+    dp[i] = i if j == 0 else dp[j-1] + i-j
 
 for i in range(n):
     dp[i] += n-i-1
