e68316895af3346b854b331d1d137d29a5326482
jxu <7989982+jxu@users.noreply.github.com>
2021-09-22 23:19:04 -0400

    Change dp solution back to O(n) space solution

--- f21week4/b.py
+++ f21week4/b.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-from collections import defaultdict
 from sys import stdin
 
 lines = [list(map(int, line.strip().split())) for line in stdin]
-n, m, k = lines[0]
+N, M, K = lines[0]
 p = lines[1]
 
-s = [0] * (n-m+1)
-s[0] = sum(p[0:m])
-for i in range(1, n-m+1):
-    s[i] = s[i-1] - p[i-1] + p[i+m-1] 
+s = [0] * (N-M+1)
+s[0] = sum(p[0:M])
+for i in range(1, N-M+1):
+    s[i] = s[i-1] - p[i-1] + p[i+M-1]  # sliding window
 
-dp = [[0] * (n-m+1) for _ in range(k)]
+dp = [0] * (N-M+1)
 
-for kk in range(k):
-    for nn in range(n-m+1):
-        dp[kk][nn] = max(s[nn] + (0 if (nn-m < 0 or kk-1 < 0) else dp[kk-1][nn-m]), 
-                0 if nn-1 < 0 else dp[kk][nn-1])
+for k in range(K):
+    new_dp = [0] * (N-M+1)
 
-print(max(dp[k-1]))
+    for n in range(N-M+1):
+        new_dp[n] = max(s[n] + (0 if (n-M < 0 or k-1 < 0) else dp[n-M]), 
+                0 if n-1 < 0 else new_dp[n-1])
+    dp = new_dp
+
+print(max(dp))
 
