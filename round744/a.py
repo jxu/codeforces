fe0040fcc120735063ecd8da00c92dc5410bae9f
jxu <7989982+jxu@users.noreply.github.com>
2021-10-01 00:37:41 -0400

    Golf Casimir's string a bit

--- round744/a.py
+++ round744/a.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env pypy3
-from sys import stdin
-
-lines = [line.strip() for line in stdin]; t = int(lines[0])  # one string per line
-for i in range(1,t+1):
-    l = lines[i]
+t = int(input())
+for _ in range(t):
+    l = input()
     print("YES" if l.count("B") == l.count("A") + l.count("C") else "NO")
