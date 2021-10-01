cfb9776c87032aae74ec387b18cb563b17a2987e
jxu <7989982+jxu@users.noreply.github.com>
2021-10-01 01:07:08 -0400

    Revert "Optimize ticks time complexity"
    This reverts commit 075efd9171a290cd69bd2068e4d194ef6a36033f.

--- round744/c.py
+++ round744/c.py
@@ -7,29 +7,21 @@ ln = 1
 for _ in range(t):
     n, m, k = map(int, lines[ln].split())
     ln += 1
-    g = [[int(c == "*") for c in lines[ln+i]] for i in range(n)]
+    g = [[1 if c == "*" else 0 for c in lines[ln+i]] for i in range(n) ]
+    
     ln += n
 
-    # reachable
-    r = [[0]*m for i in range(n)]
+    ng = [[0]*m for i in range(n)]
 
-    for i in range(k, n):
-        for j in range(k, m-k):
-            # check size k tick
-            if not all((g[i-h][j-h] and g[i-h][j+h]) for h in range(k+1)):
-                continue
+    for i in range(n):
+        for j in range(m):
+            for d in range(k, 20):
+                if not (i - d >= 0 and j - d >= 0 and j+d < m): break
+                
+                if all((g[i-h][j-h] and g[i-h][j+h]) for h in range(d+1)):
+                    for h in range(d+1):
+                        ng[i-h][j-h] = 1
+                        ng[i-h][j+h] = 1
 
-            # mark size k tick
-            for h in range(k+1):
-                r[i-h][j-h] = r[i-h][j+h] = 1
-
-            # try increasing d from k
-            d = k+1
-            while i - d >= 0 and j - d >= 0 and j+d < m and \
-                g[i-d][j-d] and g[i-d][j+d]:
-                    r[i-d][j-d] = r[i-d][j+d] = 1
-                    d += 1
-
-
-    print("YES" if g == r else "NO")
+    print("YES" if g == ng else "NO")
 
