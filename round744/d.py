9e4b3d65144b982c2762051e2750fdc39e3ad12f
jxu <7989982+jxu@users.noreply.github.com>
2021-10-01 16:16:24 -0400

    Revert "Experiment with micro-optimizing heapq"
    This reverts commit 1c20828c95d41b74cf14036bc46951cdd3c51ef9.

--- round744/d.py
+++ round744/d.py
@@ -9,18 +9,20 @@ for _ in range(t):
     n, a = lines[ln][0], lines[ln+1]
     ln += 2
     s = []
-    h = [[-a[i], i+1] for i in range(n)]
-    heapq.heapify(h)
+    m = [[-a[i], i+1] for i in range(n)]
+    heapq.heapify(m)
 
     while True:
-        a = heapq.heappop(h)
-        if h[0][0] == 0: break
+        a = heapq.heappop(m)
+        b = heapq.heappop(m)
+        if b[0] == 0: break
 
-        s.append((a[1], h[0][1]))       
+        s.append((a[1], b[1]))       
 
         a[0] += 1
-        heapq.heapreplace(h, [h[0][0] + 1, h[0][1]])
-        heapq.heappush(h, a)
+        b[0] += 1
+        heapq.heappush(m, a)
+        heapq.heappush(m, b)
         
     print(len(s))
     if len(s): print("\n".join(f"{p[0]} {p[1]}" for p in s))
