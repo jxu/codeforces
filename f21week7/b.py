bf5495b5a8bdfb74470ebc3cad2a18bae5b1dc20
jxu <7989982+jxu@users.noreply.github.com>
2021-10-13 20:20:54 -0400

    Shorten max max min using accumulate

--- f21week7/b.py
+++ f21week7/b.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env pypy3
 from sys import stdin
+from itertools import accumulate  # scan
 
 lines = [list(map(int, line.strip().split())) for line in stdin]
 n, k = lines[0]
 a = lines[1]
-n = len(a)
-a_pre = [None]*n
-a_post = [None]*n
-a_pre[0] = a[0]
-a_post[n-1] = a[n-1]
-for i in range(1, len(a)):
-    a_pre[i] = min(a_pre[i-1], a[i])
-for i in range(n-2, 0, -1):
-    a_post[i] = min(a_post[i+1], a[i]) 
 
+pre = list(accumulate(a, min))
+post = list(reversed(list(accumulate(reversed(a), min))))
 
 if k == 1:
     print(min(a))
 elif k == 2:
-    print(max(max(a_pre[i-1], a_post[i]) for i in range(1, len(a))))
+    print(max(max(pre[i-1], post[i]) for i in range(1, len(a))))
 else:
     print(max(a))
 
